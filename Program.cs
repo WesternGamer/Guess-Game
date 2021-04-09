d6cabf39dce8a95ec26c13b6135447d1870aaf52
WesternGamer <Kevnguyen474@gmail.com>
2021-04-09 19:40:04 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Hash_Tutorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string secretWord = "Western";
+            string guessWord = "";
+            int guessCount = 0;
+            int guessLimit = 5;
+            bool outOfGuesses = false;
+
+
+            while(guessWord != secretWord && !outOfGuesses)
+            {
+                if (guessCount < guessLimit)
+                {
+                    Console.Write("Enter guess: ");
+                    guessWord = Console.ReadLine();
+                    guessCount++;
+                }
+                else
+                {
+                    outOfGuesses = true;
+                }
+            }
+
+            if (outOfGuesses)
+            {
+                Console.Write("Guess Limit Reached! You Lose. :(");
+            }
+            else
+            {
+                Console.Write("You found the secret word! You win!!!");
+            }
+            Console.ReadLine();
+        }
+    }
+}
+    
